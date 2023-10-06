c5449a396d6139a76bfb7db32ede87652d2d9d02
kolnn <126846715+kolnn@users.noreply.github.com>
2023-10-06 16:09:02 +0900

    Update hw4.c

--- hw4.c
+++ hw4.c
@@ -1,12 +1,12 @@
-/*#include<stdio.h>
+#include<stdio.h>
 int main(void)
 {
 	int a;
 	printf("Please enter a number : ");
 	scanf("%d", &a);
-	if (a % 2 != 0)
+	if (a % 2 != 0 || a % 3 != 0 || a % 5 !=0 || a% 7 !=0 || a % 9 != 0 || a % 11 != 0 || a % 13 != 0 || a % 17 !=0 || a % 19 != 0 ) 
 		printf("it is a prime number");
 	else
 		printf("it is not a prime number");
 	return 0;
-}*/
+}
