59b52d0b9c1918acf3d69ba9d2f9622b51e23960
kolnn <126846715+kolnn@users.noreply.github.com>
2023-10-06 13:21:50 +0900

    Add files via upload

---
+++ hw4.c
@@ -0,0 +1,12 @@
+/*#include<stdio.h>
+int main(void)
+{
+	int a;
+	printf("Please enter a number : ");
+	scanf("%d", &a);
+	if (a % 2 != 0)
+		printf("it is a prime number");
+	else
+		printf("it is not a prime number");
+	return 0;
+}*/
